591cd741a73e509d6268aab49768e0b32814add6
Nalin Singh <154657904+NalinKumarSingh@users.noreply.github.com>
2024-08-19 20:10:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function selectionSort() {
-    window.location.href = 'http://127.0.0.1:5500/SelectionSort.html'
+    window.location.href = 'https://nalinkumarsingh.github.io/Sort_Visualizer/SelectionSort.html'
     // console.log("working");
 }
 function insertionSort() {
-    window.location.href = 'http://127.0.0.1:5500/InsertionSort.html'
+    window.location.href = 'https://nalinkumarsingh.github.io/Sort_Visualizer/InsertionSort.html'
     // console.log("working");
 }
 function bubbleSort() {
-    window.location.href = 'http://127.0.0.1:5500/BubbleSort.html'
+    window.location.href = 'https://nalinkumarsingh.github.io/Sort_Visualizer/BubbleSort.html'
     // console.log("working");
 }
 function quickSort() {
-    window.location.href = 'http://127.0.0.1:5500/QuickSort.html'
+    window.location.href = 'https://nalinkumarsingh.github.io/Sort_Visualizer/QuickSort.html'
     // console.log("working");
 }
 function mergeSort() {
-    window.location.href = 'http://127.0.0.1:5500/MergerSort.html'
+    window.location.href = 'https://nalinkumarsingh.github.io/Sort_Visualizer/MergerSort.html'
     // console.log("working");
 }
